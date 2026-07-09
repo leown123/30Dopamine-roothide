c1df43da7a17daac6e7e4ecca3895351556493ac
leown123 <136781210+leown123@users.noreply.github.com>
2026-07-09 21:49:31 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -10482,10 +10482,10 @@ if (load_executable_path() == 0)
 		//ret = DobbyHook((void *)vm_protect_ptr, (void*)hooked_vm_protect, (void**)&original_vm_protect);
 		//NSLog(@"小罪ADD: [Dobby] hook hooked_vm_protect: %s", ret == 0 ? "success" : "failed");
 
-		void *memcpy_ptr = (void *)(tersafeadd+0x249AF4);
+		//void *memcpy_ptr = (void *)(tersafeadd+0x249AF4);
 		//ret = DobbyHook((void *)memcpy, (void*)hooked_memcpy, (void**)&original_memcpy);
-		ret = DobbyHook((void *)memcpy_ptr, (void*)hooked_memcpy, (void**)&original_memcpy);
-		NSLog(@"小罪ADD: [Dobby] hook hooked_memcpy: %s", ret == 0 ? "success" : "failed");
+		//ret = DobbyHook((void *)memcpy_ptr, (void*)hooked_memcpy, (void**)&original_memcpy);
+		//NSLog(@"小罪ADD: [Dobby] hook hooked_memcpy: %s", ret == 0 ? "success" : "failed");
 
 		/*
         long tersafehookptr31 = tersafeadd + 0x74180;
